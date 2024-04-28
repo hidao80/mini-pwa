743cc6695dffece37771ab3b44242868bcb36d16
hidao80 <8155294+hidao80@users.noreply.github.com>
2024-04-28 12:08:22 +0900

    :bug:Service worker is not installed.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,10 @@
+import * as sw from "./sw_installer.js";
+
 /**
  * Add event listener to the Generate button
  */
 document.getElementById("generate").addEventListener("click", () => {
   document.getElementById("uuid").value = crypto.randomUUID();
 });
+
+sw.install();
